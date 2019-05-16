ab9b8938efbccd5483bb2dcde81c35164206feea
jbacon <julien.bacon.1@gmail.com>
2019-05-15 20:46:17 -0400

    Setting up live feed

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-card no-body>
+      <b-tabs card fill>
+        <b-tab title="Live Feed" active>
+          <LiveFeed/>
+        </b-tab>
+        <b-tab title="Time-Lapse Recorder">
+          Prout 2
+        </b-tab>
+      </b-tabs>
+    </b-card>
+</template>
+
+<script>
+import LiveFeed from './LiveFeed.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    LiveFeed,
+  },
+};
+</script>
---
+++ src/components/LiveFeed.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <img src="http://192.168.1.22:8000/stream.mjpg" width="1280" height="720" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LiveFeed',
+};
+</script>
